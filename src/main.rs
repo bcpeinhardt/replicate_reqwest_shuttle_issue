0138555b2ce508b0ada1c958797c8656b273f45f
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2022-11-01 12:23:32 -0500

    ugh

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Works
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+
+    // Fails
+    let resp = reqwest::get("https://hello_world.shuttleapp.rs/hello")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
